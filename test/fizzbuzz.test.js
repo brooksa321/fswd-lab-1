844053aea938c721ae0bce0ef72d55c0968eecc9
Aaron Brooks <brooksa321@gmail.com>
2017-09-24 11:15:20 -0400

    test

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -12,20 +12,20 @@ describe('fizzBuzz', function() {
         expect(fizzBuzz(0)).toEqual([]);
     });
 
-    // when you are feeling confident, uncomment the following tests
-    // that are based on the function description
+    //when you are feeling confident, uncomment the following tests
+    //that are based on the function description
 
-    // describe('examples from the function description', function() {
-    //     [
-    //         [0, []],
-    //         [2, ['FizzBuzz', 1]],
-    //         [7, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz']],
-    //         [16, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']]
-    //     ].forEach(function(testValues) {
-    //           it('fizzBuzz(' + testValues[0] + ')', function() {
-    //               expect(fizzBuzz(testValues[0])).toEqual(testValues[1]);
-    //           });
-    //       });
-    // });
+    describe('examples from the function description', function() {
+        [
+            [0, []],
+            [2, ['FizzBuzz', 1]],
+            [7, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz']],
+            [16, ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']]
+        ].forEach(function(testValues) {
+              it('fizzBuzz(' + testValues[0] + ')', function() {
+                  expect(fizzBuzz(testValues[0])).toEqual(testValues[1]);
+              });
+          });
+    });
 
 });
